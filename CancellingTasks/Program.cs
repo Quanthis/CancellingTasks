7754dc0681ac69fc4cdff88069cfea881d316412
Młyński Stanisław <mlynski.stanislaw@gmail.com>
2020-01-27 16:21:09 +0100

    Dodaj pliki projektów.

---
+++ CancellingTasks/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using static System.Console;
+using static System.Convert;
+
+namespace CancellingTasks
+{
+    class Program
+    {
+        public static async Task Main()
+        {
+            var tokenSource = new CancellationTokenSource();
+            CancellationToken ct = tokenSource.Token;
+
+            var t1 = Task.Run(() =>
+            {
+                ct.ThrowIfCancellationRequested();                              //in case cancellation was already requested before
+
+                bool doNotClose = true;
+                while (doNotClose)
+                {
+                    WriteLine("Executing Thread...");
+
+                    if (ct.IsCancellationRequested)
+                    {
+                        ct.ThrowIfCancellationRequested();
+                    }
+                }
+            }, tokenSource.Token);
+
+            for (int i = 0; i < 5; i++)
+            {
+                if (i == 4)
+                {
+                    tokenSource.Cancel();
+                }
+                Thread.Sleep(1000);
+            }
+
+            try
+            {
+                await t1;
+            }
+            catch(OperationCanceledException)
+            {
+                WriteLine("Operation was cancelled.");
+            }
+            catch(Exception ex)
+            {
+                WriteLine("Unpredicted exception has occured, closing app.");
+                Thread.Sleep(2000);
+                Environment.Exit(0);
+            }
+            finally
+            {
+                tokenSource.Dispose();
+            }
+            
+            ReadKey();
+        }
+    }
+}
